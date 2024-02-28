5fef9bc0c132b77c13ad0f7d986a3f11bba737ca
DevilsDomain <alaraerzincan@gmail.com>
2024-02-28 17:29:19 +0100

    created basic VideoPage compoenent with embeded video

--- src/routes/VideoPage.jsx
+++ src/routes/VideoPage.jsx
@@ -1,7 +1,22 @@
+import { useParams } from 'react-router-dom'
+import useSWR from 'swr'
+
+
+const fetcher = url => fetch(url).then(r => r.json())
 
 function VideoPage() {
+  const { videoID } = useParams();
+  const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoID}`, fetcher)
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
   return (
-    <div>VideoPage</div>
+    <div>
+        <p>{data.title}</p>
+        <iframe width="420" height="315" 
+        src={`${data.url.substring(0,24)}embed/${videoID}`}>
+        </iframe>
+        <p>{data.description}</p>
+    </div>
   )
 }
 
