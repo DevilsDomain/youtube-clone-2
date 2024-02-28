8e5fc41e7fd4447548120b1bba2a14ec0f19d71f
DevilsDomain <alaraerzincan@gmail.com>
2024-02-28 16:55:52 +0100

    basic api data fetching at homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+import useSWR from 'swr'
 
+const fetcher = url => fetch(url).then(r => r.json())
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("cats")
+
+  const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/search?q=${searchQuery}`, fetcher)
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  console.log(data[2].url)
 
   return (
     <>
       <div>
-        App.js
+        {data.map((video) => (
+          <p key={video.id}>
+            {video.title} - {video.description} - {video.url}
+          </p>
+        ))}
       </div>
     </>
   )
