5ec7575d716d63055189ac9c37e57ba26b6968b3
DevilsDomain <alaraerzincan@gmail.com>
2024-02-28 19:35:16 +0100

    added error state when loading video

--- src/routes/VideoPage.jsx
+++ src/routes/VideoPage.jsx
@@ -1,33 +1,44 @@
-import { useParams } from 'react-router-dom'
-import useSWR from 'swr'
+import { useParams } from 'react-router-dom';
+import useSWR from 'swr';
 import App from '../App';
 
-
-const fetcher = url => fetch(url).then(r => r.json())
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 function VideoPage() {
   const { videoID } = useParams();
-  const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoID}`, fetcher)
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
-  console.log(data[0])
+  const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoID}`, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  let iframeSrc;
+  try {
+    iframeSrc = `${data.url.substring(0, 24)}embed/${videoID}`;
+  } catch (err) {
+    // Handle the error, set a flag, or log it
+    console.error("Error creating iframe src:", err);
+    iframeSrc = null; // Set to null or some default value
+  }
+
   return (
     <div>
-        <div>
-            <iframe width="420" height="315" 
-            src={`${data.url.substring(0,24)}embed/${videoID}`}>
-            </iframe>
-            <p>{data.title}</p>
-            <p>{data.description}</p>
-            <p>{data.channelName}</p>
-            <p>{data.views}</p>
-        </div>
-        <div>
-            <h1>recommended videos</h1>
-            <App />
-        </div>
+      <div>
+        {iframeSrc ? (
+          <iframe width="420" height="315" src={iframeSrc}></iframe>
+        ) : (
+          <p>Error loading video</p>
+        )}
+        <p>{data.title}</p>
+        <p>{data.description}</p>
+        <p>{data.channelName}</p>
+        <p>{data.views}</p>
+      </div>
+      <div>
+        <h1>recommended videos</h1>
+        <App />
+      </div>
     </div>
-  )
+  );
 }
 
-export default VideoPage
+export default VideoPage;
