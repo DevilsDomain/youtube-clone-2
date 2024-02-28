4962b1e5c17f2d475967fb90b12d2216b75c335a
DevilsDomain <alaraerzincan@gmail.com>
2024-02-28 19:25:03 +0100

    updated the HTML for VideoPage

--- src/routes/VideoPage.jsx
+++ src/routes/VideoPage.jsx
@@ -10,19 +10,21 @@ function VideoPage() {
   const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoID}`, fetcher)
   if (error) return <div>failed to load</div>
   if (isLoading) return <div>loading...</div>
-  console.log(data)
+  console.log(data[0])
   return (
     <div>
         <div>
-            <p>{data.title}</p>
             <iframe width="420" height="315" 
             src={`${data.url.substring(0,24)}embed/${videoID}`}>
             </iframe>
+            <p>{data.title}</p>
             <p>{data.description}</p>
+            <p>{data.channelName}</p>
+            <p>{data.views}</p>
         </div>
         <div>
             <h1>recommended videos</h1>
-            <App></App>
+            <App />
         </div>
     </div>
   )
