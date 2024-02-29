9fae0a16f60dc8522d8fb119fdfc1347a2730cbb
DevilsDomain <alaraerzincan@gmail.com>
2024-02-29 10:14:28 +0100

    video component styled

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -1,16 +1,51 @@
-import { Link } from "react-router-dom"
-function Video({video}) {
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const VideoContainer = styled(Link)`
+  display: flex;
+  border-radius: 20px;
+  text-decoration: none; /* Remove text decoration from VideoContainer */
+  color: black;
+  width: 400px;
+  font-family: "Roboto", sans-serif;
+
+  div p {
+    text-decoration: none;
+    margin: 0;
+    padding-left: 10px;
+  }
+  div img {
+    width: 400px;
+    height: 200px;
+    border-radius: 20px;
+  }
+`;
+
+const Title = styled.p`
+    font-weight: 700;
+    font-style: normal;
+    padding-bottom: 5px;
+`
+const TitleThin = styled.p`
+    font-weight: 200px;
+    font-style: normal;
+    color: #606061;
+    font-size: 14px;
+    padding-bottom: 2px;
+`
+
+function Video({ video }) {
   return (
-    <Link to={`/video/${video.id.videoId}`}>
-        <div>
-            <p>{video.duration_raw}</p>
-            <img src={video.snippet.thumbnails.url} />
-            <p>{video.title}</p>
-            <p>{video.channelName}</p>
-            <p>{video.views}</p>
-        </div>
-    </Link>
-  )
+    <VideoContainer to={`/video/${video.id.videoId}`}>
+      <div>
+        {/* <duration>{video.duration_raw}</duration> */}
+        <img src={video.snippet.thumbnails.url} alt={video.title} />
+        <Title>{video.title}</Title>
+        <TitleThin>{video.channelName}</TitleThin>
+        <TitleThin>{video.views} views</TitleThin>
+      </div>
+    </VideoContainer>
+  );
 }
 
-export default Video
+export default Video;
