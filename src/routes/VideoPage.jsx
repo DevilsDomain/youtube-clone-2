c77c495bc7f962c773737608a7a5ffb0c5ce84a9
DevilsDomain <alaraerzincan@gmail.com>
2024-02-29 11:20:08 +0100

    videopage styled

--- src/routes/VideoPage.jsx
+++ src/routes/VideoPage.jsx
@@ -1,9 +1,44 @@
 import { useParams } from 'react-router-dom';
 import useSWR from 'swr';
 import App from '../App';
+import styled from 'styled-components';
+
+const VideoContainer = styled.div`
+    width: 1000px;
+    margin: 0 auto; 
+    padding-top: 50px;
+`
+
+const VideoPlayer = styled.iframe`
+    border-radius: 20px;
+    border: none;
+`
+const Title = styled.p`
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    padding-bottom: 5px;
+    font-size: 25px;
+`
+const Description = styled.p`
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+    padding-bottom: 5px;
+    font-size: 15px;
+`
+const RecVids = styled.h1`
+    font-family: "Roboto", sans-serif;
+    font-weight: 700;
+    font-style: normal;
+    padding-bottom: 5px;
+    font-size: 30px;
+    margin: 0
+`
+
+
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
-
 function VideoPage() {
   const { videoID } = useParams();
   const { data, error, isLoading } = useSWR(`https://youtube.thorsteinsson.is/api/videos/${videoID}`, fetcher);
@@ -21,19 +56,19 @@ function VideoPage() {
 
   return (
     <div>
-      <div>
+      <VideoContainer>
         {iframeSrc ? (
-          <iframe width="420" height="315" src={iframeSrc}></iframe>
+          <VideoPlayer width="1000" height="500" src={iframeSrc}></VideoPlayer>
         ) : (
           <p>Error loading video</p>
         )}
-        <p>{data.title}</p>
-        <p>{data.description}</p>
-        <p>{data.channelName}</p>
-        <p>{data.views}</p>
-      </div>
+        <Title>{data.title}</Title>
+        <Description>{data.description}</Description>
+        <Description>{data.channelName}</Description>
+        <Description>{data.views} views</Description>
+      </VideoContainer>
       <div>
-        <h1>recommended videos</h1>
+        <RecVids>recommended videos</RecVids>
         <App />
       </div>
     </div>
