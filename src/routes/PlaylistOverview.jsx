2a3d7f4766ece6395712f14f58a6dec55b0070bc
DevilsDomain <alaraerzincan@gmail.com>
2024-03-06 12:45:30 +0100

    fetching playlist videos on PlaylistOverview page correctly

--- src/routes/PlaylistOverview.jsx
+++ src/routes/PlaylistOverview.jsx
@@ -1,12 +1,26 @@
 import { useParams } from 'react-router-dom';
+import useSWR from 'swr';
+import Video from '../Components/Video';
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 function PlaylistOverview() {
   const { id, name } = useParams();
+  const { data, error, isLoading } = useSWR(
+    ` https://youtube.thorsteinsson.is/api/playlists/${id}`,
+    fetcher
+  );
 
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(data)
   return (
     <div>
       <h1>{name}</h1>
       <p>{id}</p>
+      {/* {data.map((video) => (
+        <Video video={video} key={video.id.videoId} />
+      ))} */}
     </div>
   )
 }
